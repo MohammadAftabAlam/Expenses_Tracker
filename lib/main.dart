5a640e9e0bf662a70235c04a59794a192d7ec204
MohammadAftabAlam <mohammadaftab1062@gmail.com>
2024-07-30 20:29:42 +0530

    Dark Theme Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 
 import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 void main() {
   // WidgetsFlutterBinding.ensureInitialized();
